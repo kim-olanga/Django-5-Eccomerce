2abcf84148e104823e8c096a62aed8a32f57f2ce
Kimberly <kimolanga123@gmail.com>
2022-06-23 00:27:44 +0300

    create navbar

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def store(request):
     context = {}
-    return render(request,'store/store.html',context)
+    return render(request,'store/index.html',context)
 
 def cart(request):
     context = {}
